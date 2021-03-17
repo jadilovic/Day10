b84ea3503721ee59f29bba8551fcc73d7b70e72c
jadilovic <adilovic79@yahoo.com>
2021-03-17 16:12:57 +0100

    first commit

---
+++ ex01/src/index.js
@@ -0,0 +1,18 @@
+// Only change code below this line
+var myPet = {
+    species: "Bull Dog",
+    name: "Rex",
+    legs: 4,
+    friends: ["Bob", "Linda"]
+};
+
+function myFunction(myObj){
+    return myObj;
+}
+
+
+// Only change code above this line
+
+console.log(myFunction(myPet)); // Change this line
+
+module.exports = {myPet, myFunction};
